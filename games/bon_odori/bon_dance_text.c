5a956431dc36472412a592e1e54e36881ed16de2
somethingAccurate <loading.ec@gmail.com>
2026-05-04 19:49:18 +0100

    Various Bon Odori text fixes

--- games/bon_odori/bon_dance_text.c
+++ games/bon_odori/bon_dance_text.c
@@ -23,35 +23,35 @@ const char D_0806a448[] = "The audience says...";
 
 const char text_bon_dance_ha[] = ".0:0" "‚g‚‚`";
 
-const char text_bon_dance_hanabi_agare_ba[] = ".0:0" "‚g‚‚‚‚‚‚‰@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_dance_hanabi_agare_ba[] = ".0:0" "‚g‚‚‚‚‚‚‰@‚‚‡‚‚’‚…@‚‚‚`";
 
 const char text_bon_dance_don_don_pan_pan[] = ".1:0" "‚c‚|‚@‚„‚|‚@" ".2:0" "‚‚|‚@‚‚|‚";
 
 const char text_bon_dance_dondo_pan_pan[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚|‚@‚‚‚";
 
-const char text_bon_dance_haa[] = ".0:0" "‚g‚`@‚`‚ˆ`";
+const char text_bon_dance_haa[] = ".0:0" "‚g‚‚`@‚`‚ˆ`";
 
-const char text_bon_dance_kansei_agaru[] = ".0:0" "‚j‚‚‚“‚…‚‰@‚`‚‡‚‚’‚•`";
+const char text_bon_dance_kansei_agaru[] = ".0:0" "‚j‚‚‚“‚…‚‰@‚‚‡‚‚’‚•`";
 
-const char text_bon_dance_pan_pan_dondo_don[] = ".2:0" "‚o‚‚@‚‚‚@" ".1:0" "‚„‚‚‚„‚@‚„‚‚";
+const char text_bon_dance_pan_pan_dondo_don[] = ".2:0" "‚o‚|‚@‚‚|‚@" ".1:0" "‚„‚‚‚„‚@‚„‚|‚";
 
 const char text_bon_dance_panpa_don_pan[] = ".2:0" "‚o‚‚‚‚@" ".1:0" "‚„‚|‚@" ".2:0" "‚‚‚";
 
-const char text_bon_dance_ninki_agare_ba[] = ".0:0" "‚m‚‰‚‚‹‚‰@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_dance_ninki_agare_ba[] = ".0:0" "‚m‚‰‚‚‹‚‰@‚‚‡‚‚’‚…@‚‚‚`";
 
-const char text_bon_dance_kyuryo_agaru[] = ".0:0" "‚j‚™‚•‚’‚™‚‚•@‚`‚‡‚‚’‚•`";
+const char text_bon_dance_kyuryo_agaru[] = ".0:0" "‚j‚™‚•‚•‚’‚™‚‚•@‚‚‡‚‚’‚•`";
 
-const char text_bon_dance_matsuri_da_wasshoi[] = ".0:0" "‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_dance_matsuri_da_wasshoi[] = ".0:0" "‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_dance_dondo_panpa_dondo_panpa[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚‚‚‚@" ".1:0" "‚„‚‚‚„‚@" ".2:0" "‚‚‚‚‚";
 
-const char text_bon_dance_nippon_chu_ga_wasshoi[] = ".0:0" "‚m‚‰‚‚‚‚@‚ƒ‚ˆ‚•@‚‡‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_dance_nippon_chu_ga_wasshoi[] = ".0:0" "‚m‚‰‚‚‚‚@‚ƒ‚ˆ‚•‚•@‚‡‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_dance_dondo_panpa_pan[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚‚‚‚@‚‚‚";
 
-const char text_bon_dance_sore_hikkuri_kaette[] = ".0:0" "‚r‚‚’‚…@‚g‚‰‚‹‚‹‚•‚’‚‰@‚j‚‚…‚”‚”‚…";
+const char text_bon_dance_sore_hikkuri_kaette[] = ".0:0" "‚r‚‚’‚…@‚ˆ‚‰‚‹‚‹‚•‚’‚‰@‚‹‚‚…‚”‚”‚…";
 
-const char text_bon_dance_ha_bon_odori[] = ".0:0" "‚g‚`@‚a‚‚@‚n‚„‚‚’‚‰";
+const char text_bon_dance_ha_bon_odori[] = ".0:0" "‚g‚‚`@‚a‚‚@‚n‚„‚‚’‚‰`";
 
 const char text_bon_dance_dondo_panpa[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚‚‚‚";
 
@@ -59,14 +59,14 @@ const char text_bon_dance_panpa_dondo[] = ".2:0" "‚o‚‚‚‚@" ".1:0" "‚„‚‚‚„
 
 const char text_bon_dance_pan_panpa_pa[] = ".2:0" "‚o‚|‚@‚‚‚‚‚@‚‚‚";
 
-const char text_bon_dance_ame_ga_agare_ba[] = ".0:0" "‚`‚‚…@‚‡‚@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_dance_ame_ga_agare_ba[] = ".0:0" "‚`‚‚…@‚‡‚@‚‚‡‚‚’‚…@‚‚‚`";
 
-const char text_bon_dance_yagura_ni_agaru[] = ".0:0" "‚x‚‚‡‚•‚’‚@‚‚‰@‚`‚‡‚‚’‚•`";
+const char text_bon_dance_yagura_ni_agaru[] = ".0:0" "‚x‚‚‡‚•‚’‚@‚‚‰@‚‚‡‚‚’‚•`";
 
-const char text_bon_dance_hora_matsuri_da_wasshoi[] = ".0:0" "‚g‚‚’‚I@‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_dance_hora_matsuri_da_wasshoi[] = ".0:0" "‚g‚‚’‚I@‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_dance_korezo[] = ".0:0" "‚j‚‚’‚…‚š‚";
 
-const char text_bon_dance_meido_in_japan[] = ".0:0" "‚l‚‚„‚…@‚‰‚@‚i‚" ".2" "‚o‚`‚m";
+const char text_bon_dance_meido_in_japan[] = ".0:0" "‚‚‚„‚…@‚‰‚@‚i‚" ".2" "‚o‚`‚m";
 
 const char text_bon_dance_pan[] = ".2:0" "‚o‚‚";
--- games/bon_odori/bon_odori_text.c
+++ games/bon_odori/bon_odori_text.c
@@ -23,45 +23,45 @@ const char D_08067778[] = "The audience says...";
 
 const char text_bon_odori_ha[] = ".0:0" "‚g‚‚`";
 
-const char text_bon_odori_hanabi_agare_ba[] = ".0:0" "‚g‚‚‚‚‚‚‰@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_odori_hanabi_agare_ba[] = ".0:0" "‚g‚‚‚‚‚‚‰@‚‚‡‚‚’‚…@‚‚‚`";
 
 const char text_bon_odori_don_don_pan_pan[] = ".1:0" "‚c‚|‚@‚„‚|‚@" ".2:0" "‚‚|‚@‚‚|‚";
 
 const char text_bon_odori_dondo_pan_pan[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚|‚@‚‚‚";
 
-const char text_bon_odori_haa[] = ".0:0" "‚g‚`@‚`‚ˆ`";
+const char text_bon_odori_haa[] = ".0:0" "‚g‚‚`@‚`‚ˆ`";
 
-const char text_bon_odori_kansei_agaru[] = ".0:0" "‚j‚‚‚“‚…‚‰@‚`‚‡‚‚’‚•`";
+const char text_bon_odori_kansei_agaru[] = ".0:0" "‚j‚‚‚“‚…‚‰@‚‚‡‚‚’‚•`";
 
-const char text_bon_odori_ninki_agare_ba[] = ".0:0" "‚m‚‰‚‚‹‚‰@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_odori_ninki_agare_ba[] = ".0:0" "‚m‚‰‚‚‹‚‰@‚‚‡‚‚’‚…@‚‚‚`";
 
-const char text_bon_odori_kyuryo_agaru[] = ".0:0" "‚j‚™‚•‚’‚™‚‚•@‚`‚‡‚‚’‚•`";
+const char text_bon_odori_kyuryo_agaru[] = ".0:0" "‚j‚™‚•‚•‚’‚™‚‚•@‚‚‡‚‚’‚•`";
 
-const char text_bon_odori_matsuri_da_wasshoi[] = ".0:0" "‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_odori_matsuri_da_wasshoi[] = ".0:0" "‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_odori_dondo_panpa_dondo_panpa[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚‚‚‚@" ".1:0" "‚„‚‚‚„‚@" ".2:0" "‚‚‚‚‚";
 
-const char text_bon_odori_nippon_chu_ga_wasshoi[] = ".0:0" "‚m‚‰‚‚‚‚@‚ƒ‚ˆ‚•@‚‡‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_odori_nippon_chu_ga_wasshoi[] = ".0:0" "‚m‚‰‚‚‚‚@‚ƒ‚ˆ‚•‚•@‚‡‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_odori_don_don_pan[] = ".1:0" "‚c‚|‚@‚„‚|‚@" ".2:0" "‚‚‚";
 
-const char text_bon_odori_sore_hikkuri_kaette[] = ".0:0" "‚r‚‚’‚…@‚g‚‰‚‹‚‹‚•‚’‚‰@‚j‚‚…‚”‚”‚…";
+const char text_bon_odori_sore_hikkuri_kaette[] = ".0:0" "‚r‚‚’‚…@‚ˆ‚‰‚‹‚‹‚•‚’‚‰@‚‹‚‚…‚”‚”‚…";
 
-const char text_bon_odori_ha_bon_odori[] = ".0:0" "‚g‚`@‚a‚‚@‚n‚„‚‚’‚‰";
+const char text_bon_odori_ha_bon_odori[] = ".0:0" "‚g‚‚`@‚a‚‚@‚n‚„‚‚’‚‰`";
 
 const char text_bon_odori_dondo_panpa[] = ".1:0" "‚c‚‚‚„‚@" ".2:0" "‚‚‚‚‚";
 
 const char text_bon_odori_don_pan_pan[] = ".1:0" "‚c‚|‚@" ".2:0" "‚‚|‚@‚‚‚";
 
-const char text_bon_odori_ame_ga_agare_ba[] = ".0:0" "‚`‚‚…@‚‡‚@‚`‚‡‚‚’‚…@‚‚‚`";
+const char text_bon_odori_ame_ga_agare_ba[] = ".0:0" "‚`‚‚…@‚‡‚@‚‚‡‚‚’‚…@‚‚‚`";
 
-const char text_bon_odori_yagura_ni_agaru[] = ".0:0" "‚x‚‚‡‚•‚’‚@‚‚‰@‚`‚‡‚‚’‚•`";
+const char text_bon_odori_yagura_ni_agaru[] = ".0:0" "‚x‚‚‡‚•‚’‚@‚‚‰@‚‚‡‚‚’‚•`";
 
-const char text_bon_odori_hora_matsuri_da_wasshoi[] = ".0:0" "‚g‚‚’‚I@‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚v‚‚“‚“‚ˆ‚‚‰I";
+const char text_bon_odori_hora_matsuri_da_wasshoi[] = ".0:0" "‚g‚‚’‚I@‚l‚‚”‚“‚•‚’‚‰@‚„‚@‚—‚‚“‚“‚ˆ‚‚‰I";
 
 const char text_bon_odori_korezo[] = ".0:0" "‚j‚‚’‚…‚š‚";
 
-const char text_bon_odori_meido_in_japan[] = ".0:0" "‚l‚‚„‚…@‚‰‚@‚i‚" ".2" "‚o‚`‚m";
+const char text_bon_odori_meido_in_japan[] = ".0:0" "‚‚‚„‚…@‚‰‚@‚i‚" ".2" "‚o‚`‚m";
 
 const char text_bon_odori_pan_pan[] = ".2:0" "‚o‚‚@‚‚‚";
 
