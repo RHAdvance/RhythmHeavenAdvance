511b358edb117c612b04175a49156232fd025cf9
iestyn129 <iestyn129msm@gmail.com>
2026-09-14 13:16:48 +0100

    utf8 up this left shoulder

--- src/scenes/save_editor.c
+++ src/scenes/save_editor.c
@@ -587,7 +587,7 @@ void save_editor_render_page(void) {
 
     save_editor_draw_line(SE_LINE_HINT_A, 0, ""CHAR_L_SHOULDER_BUTTON_UTF8"/"CHAR_R_SHOULDER_BUTTON_UTF8": Change Current Save Member", 8, 48);
 
-    save_editor_draw_line(SE_LINE_HINT_B, 0, "SELECT (+ ‡K): Exit (and Save)", 8, 64);
+    save_editor_draw_line(SE_LINE_HINT_B, 0, "SELECT (+ "CHAR_L_SHOULDER_BUTTON_UTF8"): Exit (and Save)", 8, 64);
 
     if (m->kind == SE_KIND_BITFLAGS && m->flagCount > 0) {
         void* ptr = save_editor_get_value_ptr(member, arrIdx, 0);
